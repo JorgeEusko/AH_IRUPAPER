0a0ddc4d970373a82d4a826e346e6415cb712ef4
JorgeEusko <jorge@euskodata.com>
2021-05-04 09:51:11 +0200

    CAMBIO SCRIPT INSTALACION SQL

--- SQL/Scripts/INSTALACION.sql
+++ SQL/Scripts/INSTALACION.sql
@@ -155,6 +155,16 @@ GO
 
 ZPERMISOS PERS_Trabajos_Estados
 GO
+
+SET IDENTITY_INSERT [dbo].[PERS_Trabajos_Estados] ON
+GO
+
+INSERT [dbo].[PERS_Trabajos_Estados] ([IdEstado], [Descrip], [IdDoc], [InsertUpdate], [Usuario], [FechaInsertUpdate]) VALUES (1, N'Pendiente', 1, 0, N'ahora', CAST(N'2021-02-23T09:04:06.007' AS DateTime))
+INSERT [dbo].[PERS_Trabajos_Estados] ([IdEstado], [Descrip], [IdDoc], [InsertUpdate], [Usuario], [FechaInsertUpdate]) VALUES (2, N'En proceso', 2, 0, N'ahora', CAST(N'2021-02-23T09:04:10.507' AS DateTime))
+INSERT [dbo].[PERS_Trabajos_Estados] ([IdEstado], [Descrip], [IdDoc], [InsertUpdate], [Usuario], [FechaInsertUpdate]) VALUES (3, N'Terminado', 3, 0, N'ahora', CAST(N'2021-02-23T09:04:14.793' AS DateTime))
+
+SET IDENTITY_INSERT [dbo].[PERS_Trabajos_Estados] OFF
+GO
 -------------------------------------------------------------------------------------------
 -- PERS_BOBINAS
 -------------------------------------------------------------------------------------------
